f89a0c87915e9868f5c406a2c9e5b7788d38f423
Pietro Gori <pietro_gori@hotmail.com>
2018-07-17 23:42:27 -0300

    Entidades Usuario e Movimentacao; FinancasContext; Migration; e View Inicial

---
+++ Financas/DAO/FinancasContext.cs
@@ -0,0 +1,21 @@
+﻿using Financas.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Financas.DAO
+{
+    public class FinancasContext : DbContext
+    {
+        public DbSet<Usuario> Usuarios { get; set; }
+        public DbSet<Movimentacao> Movimentacoes { get; set; }
+
+        //metodo usado para informar que a entidade movimentacao possui obrigatoriamente um usuario
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Movimentacao>().HasRequired(m => m.Usuario);
+        }
+    }
+}
