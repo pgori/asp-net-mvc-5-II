fb92fa8941df282882001b221f94bbf6b94b6368
Pietro Gori <pietro_gori@hotmail.com>
2018-07-23 00:26:10 -0300

    Autenticação com Simple Membership

---
+++ Financas/Controllers/LoginController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebMatrix.WebData;
+
+namespace Financas.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Autentica(String login, String senha)
+        {
+            try
+            {
+                if (WebSecurity.Login(login, senha))
+                {
+                    return RedirectToAction("Index", "Movimentacao");
+                }
+                else
+                {
+                    return View("Index");
+                }
+            }
+            catch (Exception e)
+            {
+                ModelState.AddModelError("login.Invalido", e.Message);
+                return View("Index");
+            }
+        }
+
+        public ActionResult Logout()
+        {
+            WebSecurity.Logout();
+            return RedirectToAction("Index");
+        }
+    }
+}
